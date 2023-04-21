3304ee19104bdbde9dbeb8b9e340558254900867
Kentaro Kuribayashi <kentarok@gmail.com>
2023-04-21 20:20:21 +0900

    mix new onmyoji --sup

---
+++ lib/onmyoji.ex
@@ -0,0 +1,18 @@
+defmodule Onmyoji do
+  @moduledoc """
+  Documentation for `Onmyoji`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Onmyoji.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/onmyoji_test.exs
@@ -0,0 +1,8 @@
+defmodule OnmyojiTest do
+  use ExUnit.Case
+  doctest Onmyoji
+
+  test "greets the world" do
+    assert Onmyoji.hello() == :world
+  end
+end
